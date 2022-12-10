e4635eb6971f23d29578a1309ee71888769e9713
Mixss <michalziemiec@wp.pl>
2022-12-10 11:14:16 +0100

    Removed unnecessary functions

--- include/record.h
+++ include/record.h
@@ -12,9 +12,6 @@ struct record
     int x;
 } __attribute__((packed)); 
 
-/* computes value of the function g(x)=a0+a1x +a2x2+a3x3+a4x4*/
-int get_record_sorting_value(struct record rec);
-
 /*fills record with zeros*/
 void zero_record(struct record* rec);
 
