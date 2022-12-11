c8ab25a654fa6cfacc1ec657783ec53c89f6d6bc
Mixss <michalziemiec@wp.pl>
2022-12-11 15:57:03 +0100

    Added parent index to page

--- include/page.h
+++ include/page.h
@@ -2,10 +2,11 @@
 #define PAGE_H
 
 #define PAGE_SIZE (sizeof(struct page) + sizeof(struct page_entry) * 2 * order)
+#define NIL -1
 
 struct page_entry
 {
-    struct page *other_page;
+    int other_page;
     int key;
     int address_to_data;
 } __attribute__((packed));
@@ -13,10 +14,13 @@ struct page_entry
 struct page
 {
     int records_on_page;
+    int parent_page;
+    int next_page;
     struct page_entry entries[];
 } __attribute__((packed)); 
 
 struct page *page_init(struct page *p, int order);
 void page_free(struct page *p);
+void set_entry(struct page *p, int entry_index, int key, int address_to_data, int other_page);
 
 #endif
--- src/page.c
+++ src/page.c
@@ -5,11 +5,19 @@ struct page *page_init(struct page *p, int order)
 {
     p = malloc( PAGE_SIZE );
     p->records_on_page = 0;
-
+    p->next_page = NIL;
+    p->parent_page = NIL;
     return p;
 }
 
 void page_free(struct page *p)
 {
     free(p);
+}
+
+void set_entry(struct page *p, int entry_index, int key, int address_to_data, int other_page)
+{
+    p->entries[entry_index].key = key;
+    p->entries[entry_index].address_to_data = address_to_data;
+    p->entries[entry_index].other_page = other_page;
 }
